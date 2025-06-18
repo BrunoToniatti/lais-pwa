e2e0c84c9648d31deb6c5796d24385c7cccfa118
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-18 15:26:51 -0300

    Ajsutando endpoint

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ServiceService {
-  private apiUrl = 'http://127.0.0.1:8000/api/servicos/'; // 🔁 ajuste para seu backend real
+  private apiUrl = 'https://api.laisbarretoribeiro.com/api/servicos/'; // 🔁 ajuste para seu backend real
 
   constructor(private http: HttpClient) {}
 
