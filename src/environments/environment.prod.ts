1067ba47e4741f57b86ffbb313d1d6c50be4d8c4
Bruno Toniatti <toniattibruno08@gmail.com>
2025-07-30 17:47:20 -0300

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://31.97.168.107:8001/api/',
+  apiUrl: 'https://apihomologa.laisbarretoribeiro.com/api/',
   token: '1759a5c6ea101b459351248ce3c2ae406f7e9ab9'
 };
