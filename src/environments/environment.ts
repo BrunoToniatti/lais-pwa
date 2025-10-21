d690c3d738b6c301b75c6d659d54ac54b77bba55
Bruno Toniatti <toniattibruno08@gmail.com>
2025-10-20 21:57:20 -0300

    Ajustando token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/',
-  token: 'cc18ff3be38f470e043489c0c911efc9fdf83614'
+  token: 'b0e3fe7f69323442c13d0f9cdce357fbee67c655'
 };
